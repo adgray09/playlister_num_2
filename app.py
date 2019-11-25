531e19a9e20ec7a49a262692a43d30f5eaed7012
adgray09 <alexandergray37@yahoo.com>
2019-11-24 17:09:36 -0800

    fixing heroku

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
 
-host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/playlister2')
+host = os.getenv('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 playlists = db.playlists
